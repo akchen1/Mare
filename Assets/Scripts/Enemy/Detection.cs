dfbc332d9c267cedcd5270033e0e82c1799d4b36
Gabriel Chan <gaibryol@gmail.com>
2022-01-29 00:02:09 -0700

    add anim

--- Assets/Scripts/Enemy/Detection.cs
+++ Assets/Scripts/Enemy/Detection.cs
@@ -35,10 +35,25 @@ public class Detection : MonoBehaviour
             
         }
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+
+            RaycastHit2D hit = Physics2D.Linecast(dad.transform.position, player.transform.position);
+            if (hit.collider != null && hit.collider.name == "Player")
+            {
+                dad.setSpeed(0);
+
+            }
+
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         dad.setSpeed(1);
 
     }
-
 }
