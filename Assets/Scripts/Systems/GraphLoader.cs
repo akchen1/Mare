1ee0958ad3d8aa529fb561497fcc91b1e7db83e8
akchen1 <43446512+akchen1@users.noreply.github.com>
2022-12-22 14:20:42 -0700

    upgraded version to lts and fixed android input bug

--- Assets/Scripts/Systems/GraphLoader.cs
+++ Assets/Scripts/Systems/GraphLoader.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.Networking;
 
 public class GraphLoader : MonoBehaviour
 {
@@ -14,9 +15,35 @@ public class GraphLoader : MonoBehaviour
 
     private void LoadGraph()
     {
-        
+
+        #if UNITY_STANDALONE
         byte[] file = File.ReadAllBytes(Application.streamingAssetsPath + FilePath);
         AstarPath.active.data.DeserializeGraphs(file);
         AstarPath.active.Scan();
+        #endif
+
+        #if (UNITY_ANDROID || UNITY_IPHONE)
+        StartCoroutine(FetchFile(Application.streamingAssetsPath + FilePath));
+        #endif
+    }
+
+    private IEnumerator FetchFile(string filePath)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(filePath))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                // Get the file contents as a byte array
+                byte[] fileContent = www.downloadHandler.data;
+                AstarPath.active.data.DeserializeGraphs(fileContent);
+                AstarPath.active.Scan();
+            }
+        }
     }
 }
